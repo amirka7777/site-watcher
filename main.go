6699c00c9e4d8e4c99d4608c0df2cecd833516c4
amirka7777 <amirmgoian0808@gmail.com>
2026-06-08 21:39:57 +0700

    changed gitignore and wrote main.go for the POST request

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"site-watcher/controllers"
+	"site-watcher/database"
+	"site-watcher/repository"
+)
+
+
+func main() {
+
+	db, err := database.InitDB("sites.db")
+	if err != nil {
+		log.Fatalf("Ошибка при открытии бД: %v", err)
+	}
+
+	defer db.Close()
+
+	siteRepo := repository.NewSiteRepository(db)
+	siteContr := controllers.NewSiteController(siteRepo)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /sites", siteContr.CreateSiteHandler)
+
+	log.Println("Сервер запущен на порту 8080")
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("Ошибка при запуске сервера: %v", err)
+	}
+
+	
+
+}
